8aaed1350e628e3f9bf769d954227028c0b22bfd
ptkmartynov <kmartynov@ptsecurity.com>
2022-07-05 16:52:49 +0700

    Задание №10

--- fixtures/application.py
+++ fixtures/application.py
@@ -40,7 +40,8 @@ class Application:
 
     def go_to_home(self):
         wd = self.wd
-        wd.find_element_by_xpath("//li[1]/a").click()
+        if not (wd.find_element_by_xpath("//div[4]/div/form/input") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_xpath("//li[1]/a").click()
 
     def destroy(self):
         self.wd.quit()
