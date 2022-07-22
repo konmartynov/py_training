089d0cbd0f2edb01b01657231597b37db179cc8d
ptkmartynov <kmartynov@ptsecurity.com>
2022-07-22 13:15:49 +0700

    fix Задания №14

--- tests/test_compare_user_data.py
+++ tests/test_compare_user_data.py
@@ -30,9 +30,8 @@ def clear(s):
 
 def merge_emails_like_on_home_page(user):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [user.email1, user.email2, user.email3]))))
+                            filter(lambda x: x is not None,
+                                   [user.email1, user.email2, user.email3])))
 
 def merge_phones_like_on_home_page(user):
     return "\n".join(filter(lambda x: x != "",
