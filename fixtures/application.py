f0f3b8c995a6a6e15dc2c10a06344562e8a9be77
ptkmartynov <kmartynov@ptsecurity.com>
2022-07-22 16:24:15 +0700

    Задание №16

--- fixtures/application.py
+++ fixtures/application.py
@@ -8,12 +8,20 @@ from fixtures.user import UserHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(3)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.user = UserHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -32,7 +40,7 @@ class Application:
 
     def open_homepage(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def return_to_homepage(self):
         wd = self.wd
