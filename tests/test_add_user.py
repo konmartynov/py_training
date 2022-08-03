cf93648bfc1be683910470df2e72da0ad9cd8c75
ptkmartynov <kmartynov@ptsecurity.com>
2022-08-03 16:00:18 +0700

    Задание №19 fix

--- tests/test_add_user.py
+++ tests/test_add_user.py
@@ -2,13 +2,13 @@
 from models.user import User
 
 
-def test_add_user(app, json_user):
+def test_add_user(app, json_users):
     old_users = app.user.get_user_list()
     app.user.jump_to_add_new_user_form()
-    app.user.fill_user_form(json_user)
+    app.user.fill_user_form(json_users)
     app.user.create_new_user()
     app.return_to_homepage()
     new_users = app.user.get_user_list()
     assert len(old_users) + 1 == app.user.user_count()
-    old_users.append(User(fname=json_user.fname, lname=json_user.lname, id=json_user.id))
+    old_users.append(User(fname=json_users.fname, lname=json_users.lname, id=json_users.id))
     assert sorted(old_users, key=User.sort_by_id) == sorted(new_users, key=User.sort_by_id)
